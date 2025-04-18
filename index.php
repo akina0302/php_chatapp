9c479b6934e00c8604016e547a81fd564d44fc0c
akina0302 <ymu.m24@gmail.com>
2025-04-18 23:03:28 +0900

    Initial commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php 
+
+if(!empty($_POST["button"])){
+echo $_POST["username"];
+echo $_POST["comment"];
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP 掲示板</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <h1 class="title">PHP掲示板</h1>
+  <hr>
+  <div class="boardWrapper">
+    <section>
+    <article>
+      <div class="wrapper">
+      <div class="nameArea">
+        <span>名前:</span>
+        <p class="username">name</p>
+        <time>2025/4/17</time>
+      </div>
+      <p class="comment">手書きコメント</p>
+    </div>
+    </article>
+    </section>
+
+    <form class="formwrapper" method="POST">
+      <div>
+        <input type="submit" value="書き込む" name="button">
+        <label for="">名前</label>
+        <input type="text" name="username">
+      </div>
+      <div>
+        <textarea name="comment" class="commentTextArea"></textarea>
+      </div>
+    </form>
+  </div>
+  
+</body>
+</html>
